cb7d0ffbd46113ba748b3b842e090954467fe84f
Ozan Can Acar <129222906+OzanCanAcarSoft@users.noreply.github.com>
2024-06-02 15:12:45 +0300

    Create history2_widget.dart

---
+++ lib/pages/history2_widget.dart
@@ -0,0 +1,262 @@
+import '/auth/firebase_auth/auth_util.dart';
+import '/backend/backend.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+import 'package:provider/provider.dart';
+
+import 'history2_model.dart';
+export 'history2_model.dart';
+
+class History2Widget extends StatefulWidget {
+  const History2Widget({super.key});
+
+  @override
+  State<History2Widget> createState() => _History2WidgetState();
+}
+
+class _History2WidgetState extends State<History2Widget> {
+  late History2Model _model;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => History2Model());
+  }
+
+  @override
+  void dispose() {
+    _model.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => _model.unfocusNode.canRequestFocus
+          ? FocusScope.of(context).requestFocus(_model.unfocusNode)
+          : FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        key: scaffoldKey,
+        backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+        appBar: AppBar(
+          backgroundColor: FlutterFlowTheme.of(context).primary,
+          automaticallyImplyLeading: false,
+          title: Text(
+            'Diary History',
+            style: FlutterFlowTheme.of(context).headlineMedium.override(
+                  fontFamily: 'Outfit',
+                  color: Colors.white,
+                  fontSize: 22,
+                  letterSpacing: 0,
+                ),
+          ),
+          actions: [],
+          centerTitle: false,
+          elevation: 2,
+        ),
+        body: SafeArea(
+          top: true,
+          child: Stack(
+            children: [
+              Align(
+                alignment: AlignmentDirectional(0, 0),
+                child: Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(2, 70, 2, 0),
+                  child: StreamBuilder<List<DairyRecord>>(
+                    stream: queryDairyRecord(
+                      queryBuilder: (dairyRecord) => dairyRecord
+                          .where(
+                            'userId',
+                            isEqualTo: currentUserUid,
+                          )
+                          .orderBy('entryDate', descending: true),
+                    ),
+                    builder: (context, snapshot) {
+                      // Customize what your widget looks like when it's loading.
+                      if (!snapshot.hasData) {
+                        return Center(
+                          child: SizedBox(
+                            width: 50,
+                            height: 50,
+                            child: CircularProgressIndicator(
+                              valueColor: AlwaysStoppedAnimation<Color>(
+                                FlutterFlowTheme.of(context).primary,
+                              ),
+                            ),
+                          ),
+                        );
+                      }
+                      List<DairyRecord> gridViewDairyRecordList =
+                          snapshot.data!;
+                      return GridView.builder(
+                        padding: EdgeInsets.fromLTRB(
+                          0,
+                          6,
+                          0,
+                          0,
+                        ),
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 5,
+                          mainAxisSpacing: 10,
+                          childAspectRatio: 1,
+                        ),
+                        scrollDirection: Axis.vertical,
+                        itemCount: gridViewDairyRecordList.length,
+                        itemBuilder: (context, gridViewIndex) {
+                          final gridViewDairyRecord =
+                              gridViewDairyRecordList[gridViewIndex];
+                          return Padding(
+                            padding: EdgeInsets.all(3),
+                            child: Container(
+                              width: double.infinity,
+                              height: double.infinity,
+                              decoration: BoxDecoration(
+                                color: FlutterFlowTheme.of(context)
+                                    .secondaryBackground,
+                                boxShadow: [
+                                  BoxShadow(
+                                    blurRadius: 3,
+                                    color: Color(0x33000000),
+                                    offset: Offset(
+                                      0.0,
+                                      2,
+                                    ),
+                                  )
+                                ],
+                                borderRadius: BorderRadius.circular(0),
+                                shape: BoxShape.rectangle,
+                              ),
+                              child: Padding(
+                                padding: EdgeInsets.all(4),
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(8),
+                                      child: Image.network(
+                                        gridViewDairyRecord.photoUrl,
+                                        width: double.infinity,
+                                        height: 129,
+                                        fit: BoxFit.scaleDown,
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsetsDirectional.fromSTEB(
+                                          0, 5, 0, 0),
+                                      child: Text(
+                                        gridViewDairyRecord.emotion,
+                                        style: FlutterFlowTheme.of(context)
+                                            .bodyLarge
+                                            .override(
+                                              fontFamily: 'Readex Pro',
+                                              color: valueOrDefault<Color>(
+                                                () {
+                                                  if (gridViewDairyRecord
+                                                          .emotion ==
+                                                      'happy') {
+                                                    return Color(0xFF39AE10);
+                                                  } else if (gridViewDairyRecord
+                                                          .emotion ==
+                                                      'sad') {
+                                                    return Color(0xFFFF0003);
+                                                  } else if (gridViewDairyRecord
+                                                          .emotion ==
+                                                      'angry') {
+                                                    return Color(0xFFFF0000);
+                                                  } else if (gridViewDairyRecord
+                                                          .emotion ==
+                                                      'disgust') {
+                                                    return Color(0xFFFFCC00);
+                                                  } else {
+                                                    return FlutterFlowTheme.of(
+                                                            context)
+                                                        .primaryText;
+                                                  }
+                                                }(),
+                                                FlutterFlowTheme.of(context)
+                                                    .primaryText,
+                                              ),
+                                              letterSpacing: 0,
+                                            ),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsetsDirectional.fromSTEB(
+                                          0, 5, 0, 0),
+                                      child: Text(
+                                        dateTimeFormat(
+                                          'd/M/y h:mm',
+                                          gridViewDairyRecord.entryDate!,
+                                          locale: FFLocalizations.of(context)
+                                              .languageCode,
+                                        ),
+                                        style: FlutterFlowTheme.of(context)
+                                            .bodyMedium
+                                            .override(
+                                              fontFamily: 'Readex Pro',
+                                              letterSpacing: 0,
+                                            ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ),
+              Align(
+                alignment: AlignmentDirectional(0, -1),
+                child: Padding(
+                  padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
+                  child: LinearPercentIndicator(
+                    percent: 0.5,
+                    width: 390,
+                    lineHeight: 40,
+                    animation: true,
+                    animateFromLastPercent: true,
+                    progressColor: Color(0xFFDF9A08),
+                    backgroundColor: FlutterFlowTheme.of(context).accent4,
+                    center: Text(
+                      '50%',
+                      style:
+                          FlutterFlowTheme.of(context).headlineSmall.override(
+                                fontFamily: 'Outfit',
+                                letterSpacing: 0,
+                              ),
+                    ),
+                    barRadius: Radius.circular(20),
+                    padding: EdgeInsets.zero,
+                  ),
+                ),
+              ),
+              Align(
+                alignment: AlignmentDirectional(-0.03, -1),
+                child: Text(
+                  'General Mental Status',
+                  style: FlutterFlowTheme.of(context).bodyMedium.override(
+                        fontFamily: 'Readex Pro',
+                        letterSpacing: 0,
+                      ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
