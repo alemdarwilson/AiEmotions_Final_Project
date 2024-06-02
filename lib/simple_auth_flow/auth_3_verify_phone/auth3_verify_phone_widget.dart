3128ad2bb1cf5911f143c8e20ed86bc855ef3d17
Ozan Can Acar <129222906+OzanCanAcarSoft@users.noreply.github.com>
2024-06-02 15:05:05 +0300

    Update auth3_verify_phone_widget.dart

--- lib/simple_auth_flow/auth_3_verify_phone/auth3_verify_phone_widget.dart
+++ lib/simple_auth_flow/auth_3_verify_phone/auth3_verify_phone_widget.dart
@@ -12,6 +12,7 @@ import 'package:flutter/scheduler.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
+
 import 'auth3_verify_phone_model.dart';
 export 'auth3_verify_phone_model.dart';
 
@@ -100,7 +101,7 @@ class _Auth3VerifyPhoneWidgetState extends State<Auth3VerifyPhoneWidget>
                 Expanded(
                   flex: 5,
                   child: Align(
-                    alignment: AlignmentDirectional(0.0, -1.0),
+                    alignment: AlignmentDirectional(0, -1),
                     child: Container(
                       width: double.infinity,
                       height: double.infinity,
@@ -110,11 +111,11 @@ class _Auth3VerifyPhoneWidgetState extends State<Auth3VerifyPhoneWidget>
                             FlutterFlowTheme.of(context).primaryBackground,
                             FlutterFlowTheme.of(context).accent1
                           ],
-                          stops: [0.0, 1.0],
-                          begin: AlignmentDirectional(1.0, 0.0),
-                          end: AlignmentDirectional(-1.0, 0),
+                          stops: [0, 1],
+                          begin: AlignmentDirectional(1, 0),
+                          end: AlignmentDirectional(-1, 0),
                         ),
-                        borderRadius: BorderRadius.circular(0.0),
+                        borderRadius: BorderRadius.circular(0),
                       ),
                     ),
                   ),
@@ -122,21 +123,21 @@ class _Auth3VerifyPhoneWidgetState extends State<Auth3VerifyPhoneWidget>
               Expanded(
                 flex: 5,
                 child: Align(
-                  alignment: AlignmentDirectional(0.0, 0.0),
+                  alignment: AlignmentDirectional(0, 0),
                   child: Padding(
-                    padding: EdgeInsets.all(16.0),
+                    padding: EdgeInsets.all(16),
                     child: ClipRRect(
-                      borderRadius: BorderRadius.circular(16.0),
+                      borderRadius: BorderRadius.circular(16),
                       child: Container(
                         width: double.infinity,
                         height: double.infinity,
                         constraints: BoxConstraints(
-                          maxWidth: 570.0,
+                          maxWidth: 570,
                         ),
                         decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(16.0),
+                          borderRadius: BorderRadius.circular(16),
                         ),
-                        alignment: AlignmentDirectional(0.0, -1.0),
+                        alignment: AlignmentDirectional(0, -1),
                         child: SingleChildScrollView(
                           child: Column(
                             mainAxisSize: MainAxisSize.max,
@@ -144,31 +145,31 @@ class _Auth3VerifyPhoneWidgetState extends State<Auth3VerifyPhoneWidget>
                             children: [
                               Container(
                                 width: double.infinity,
-                                height: 140.0,
+                                height: 140,
                                 decoration: BoxDecoration(
                                   borderRadius: BorderRadius.only(
-                                    bottomLeft: Radius.circular(16.0),
-                                    bottomRight: Radius.circular(16.0),
-                                    topLeft: Radius.circular(0.0),
-                                    topRight: Radius.circular(0.0),
+                                    bottomLeft: Radius.circular(16),
+                                    bottomRight: Radius.circular(16),
+                                    topLeft: Radius.circular(0),
+                                    topRight: Radius.circular(0),
                                   ),
                                 ),
-                                alignment: AlignmentDirectional(-1.0, 0.0),
+                                alignment: AlignmentDirectional(-1, 0),
                                 child: Padding(
                                   padding: EdgeInsetsDirectional.fromSTEB(
-                                      16.0, 0.0, 16.0, 0.0),
+                                      16, 0, 16, 0),
                                   child: Row(
                                     mainAxisSize: MainAxisSize.max,
                                     mainAxisAlignment: MainAxisAlignment.start,
                                     children: [
                                       Padding(
                                         padding: EdgeInsetsDirectional.fromSTEB(
-                                            0.0, 0.0, 12.0, 0.0),
+                                            0, 0, 12, 0),
                                         child: Icon(
                                           Icons.flourescent_rounded,
                                           color: FlutterFlowTheme.of(context)
                                               .primary,
-                                          size: 44.0,
+                                          size: 44,
                                         ),
                                       ),
                                       Text(
@@ -177,7 +178,7 @@ class _Auth3VerifyPhoneWidgetState extends State<Auth3VerifyPhoneWidget>
                                             .displaySmall
                                             .override(
                                               fontFamily: 'Outfit',
-                                              letterSpacing: 0.0,
+                                              letterSpacing: 0,
                                             ),
                                       ),
                                     ],
@@ -185,10 +186,10 @@ class _Auth3VerifyPhoneWidgetState extends State<Auth3VerifyPhoneWidget>
                                 ),
                               ),
                               Align(
-                                alignment: AlignmentDirectional(0.0, 0.0),
+                                alignment: AlignmentDirectional(0, 0),
                                 child: Padding(
                                   padding: EdgeInsetsDirectional.fromSTEB(
-                                      16.0, 0.0, 16.0, 0.0),
+                                      16, 0, 16, 0),
                                   child: Column(
                                     mainAxisSize: MainAxisSize.max,
                                     crossAxisAlignment:
@@ -196,14 +197,14 @@ class _Auth3VerifyPhoneWidgetState extends State<Auth3VerifyPhoneWidget>
                                     children: [
                                       Padding(
                                         padding: EdgeInsetsDirectional.fromSTEB(
-                                            0.0, 0.0, 0.0, 12.0),
+                                            0, 0, 0, 12),
                                         child: FlutterFlowIconButton(
                                           borderColor:
                                               FlutterFlowTheme.of(context)
                                                   .secondaryBackground,
-                                          borderRadius: 12.0,
-                                          borderWidth: 1.0,
-                                          buttonSize: 40.0,
+                                          borderRadius: 12,
+                                          borderWidth: 1,
+                                          buttonSize: 40,
                                           fillColor:
                                               FlutterFlowTheme.of(context)
                                                   .secondaryBackground,
@@ -211,7 +212,7 @@ class _Auth3VerifyPhoneWidgetState extends State<Auth3VerifyPhoneWidget>
                                             Icons.arrow_back_rounded,
                                             color: FlutterFlowTheme.of(context)
                                                 .primaryText,
-                                            size: 24.0,
+                                            size: 24,
                                           ),
                                           onPressed: () async {
                                             context.safePop();
@@ -224,12 +225,12 @@ class _Auth3VerifyPhoneWidgetState extends State<Auth3VerifyPhoneWidget>
                                             .displaySmall
                                             .override(
                                               fontFamily: 'Outfit',
-                                              letterSpacing: 0.0,
+                                              letterSpacing: 0,
                                             ),
                                       ),
                                       Padding(
                                         padding: EdgeInsetsDirectional.fromSTEB(
-                                            0.0, 4.0, 0.0, 24.0),
+                                            0, 4, 0, 24),
                                         child: InkWell(
                                           splashColor: Colors.transparent,
                                           focusColor: Colors.transparent,
@@ -268,7 +269,7 @@ class _Auth3VerifyPhoneWidgetState extends State<Auth3VerifyPhoneWidget>
                                                       .override(
                                                         fontFamily:
                                                             'Readex Pro',
-                                                        letterSpacing: 0.0,
+                                                        letterSpacing: 0,
                                                         lineHeight: 1.5,
                                                       ),
                                             ),
@@ -283,14 +284,14 @@ class _Auth3VerifyPhoneWidgetState extends State<Auth3VerifyPhoneWidget>
                                             .bodyLarge
                                             .override(
                                               fontFamily: 'Readex Pro',
-                                              letterSpacing: 0.0,
+                                              letterSpacing: 0,
                                             ),
                                         mainAxisAlignment:
                                             MainAxisAlignment.spaceBetween,
                                         enableActiveFill: true,
                                         autoFocus: true,
                                         enablePinAutofill: true,
-                                        errorTextSpace: 16.0,
+                                        errorTextSpace: 16,
                                         showCursor: true,
                                         cursorColor:
                                             FlutterFlowTheme.of(context)
@@ -299,14 +300,14 @@ class _Auth3VerifyPhoneWidgetState extends State<Auth3VerifyPhoneWidget>
                                         hintCharacter: '-',
                                         keyboardType: TextInputType.number,
                                         pinTheme: PinTheme(
-                                          fieldHeight: 48.0,
-                                          fieldWidth: 48.0,
-                                          borderWidth: 2.0,
+                                          fieldHeight: 48,
+                                          fieldWidth: 48,
+                                          borderWidth: 2,
                                           borderRadius: BorderRadius.only(
-                                            bottomLeft: Radius.circular(12.0),
-                                            bottomRight: Radius.circular(12.0),
-                                            topLeft: Radius.circular(12.0),
-                                            topRight: Radius.circular(12.0),
+                                            bottomLeft: Radius.circular(12),
+                                            bottomRight: Radius.circular(12),
+                                            topLeft: Radius.circular(12),
+                                            topRight: Radius.circular(12),
                                           ),
                                           shape: PinCodeFieldShape.box,
                                           activeColor:
@@ -337,12 +338,11 @@ class _Auth3VerifyPhoneWidgetState extends State<Auth3VerifyPhoneWidget>
                                             .asValidator(context),
                                       ),
                                       Align(
-                                        alignment:
-                                            AlignmentDirectional(1.0, -1.0),
+                                        alignment: AlignmentDirectional(1, -1),
                                         child: Padding(
                                           padding:
                                               EdgeInsetsDirectional.fromSTEB(
-                                                  0.0, 0.0, 0.0, 16.0),
+                                                  0, 0, 0, 16),
                                           child: FFButtonWidget(
                                             onPressed: () async {
                                               GoRouter.of(context)
@@ -375,12 +375,12 @@ class _Auth3VerifyPhoneWidgetState extends State<Auth3VerifyPhoneWidget>
                                             },
                                             text: 'Verify Code',
                                             options: FFButtonOptions(
-                                              width: 200.0,
-                                              height: 44.0,
+                                              width: 200,
+                                              height: 44,
                                               padding: EdgeInsetsDirectional
-                                                  .fromSTEB(0.0, 0.0, 0.0, 0.0),
+                                                  .fromSTEB(0, 0, 0, 0),
                                               iconPadding: EdgeInsetsDirectional
-                                                  .fromSTEB(0.0, 0.0, 0.0, 0.0),
+                                                  .fromSTEB(0, 0, 0, 0),
                                               color:
                                                   FlutterFlowTheme.of(context)
                                                       .primary,
@@ -391,15 +391,15 @@ class _Auth3VerifyPhoneWidgetState extends State<Auth3VerifyPhoneWidget>
                                                         fontFamily:
                                                             'Readex Pro',
                                                         color: Colors.white,
-                                                        letterSpacing: 0.0,
+                                                        letterSpacing: 0,
                                                       ),
-                                              elevation: 3.0,
+                                              elevation: 3,
                                               borderSide: BorderSide(
                                                 color: Colors.transparent,
-                                                width: 1.0,
+                                                width: 1,
                                               ),
                                               borderRadius:
-                                                  BorderRadius.circular(12.0),
+                                                  BorderRadius.circular(12),
                                             ),
                                           ),
                                         ),
